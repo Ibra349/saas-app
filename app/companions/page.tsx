f66516274db6fd398ec7343d976009e52a347d16
ibra349 <foufa.ibra30@yahoo.com>
2025-05-28 18:22:22 +0200

    Initiliazing the Saas Project

---
+++ app/companions/page.tsx
@@ -0,0 +1,7 @@
+const SignIn = () => {
+    return (
+        <div>Companions</div>
+    );
+}
+
+export default SignIn
---
+++ app/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import {Button} from "@/components/ui/button";
+
+const CompanionsLibrary = () => {
+    return (
+        <div>
+            <Button className="btn-mic bg-amber-300">Sign In</Button>
+        </div>
+    )
+}
+
+export default CompanionsLibrary
+
